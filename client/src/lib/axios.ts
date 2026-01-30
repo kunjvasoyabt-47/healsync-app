954c4f7cb7f4ce303b0c7af5d49efcd9645b5254
kunjvasoyabt-47 <kunj.v@bitontree.com>
2026-01-30 11:02:46 +0530

    edit axios.ts

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // Replace with your actual Render URL
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: "https://healsync-app.onrender.com/api", //process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true, // Required to send HttpOnly cookies
 });
 
