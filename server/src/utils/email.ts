442bbd9dffeda95d5f2c5b442fc7058ce383a085
kunjvasoyabt-47 <kunj.v@bitontree.com>
2026-01-30 11:16:06 +0530

    edit email.ts

--- server/src/utils/email.ts
+++ server/src/utils/email.ts
@@ -20,6 +20,10 @@ export const sendEmail = async (options: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+
+  connectionTimeout: 10000, 
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   });
 
   const mailOptions = {
