9135581b1ffb2d8c4558f8d847b3810df5b5b9cb
kunjvasoyabt-47 <kunj.v@bitontree.com>
2026-02-10 18:01:26 +0530

    change sendemail port

--- server/src/utils/email.ts
+++ server/src/utils/email.ts
@@ -9,7 +9,7 @@ interface EmailOptions {
 export const sendEmail = async (options: EmailOptions): Promise<void> => {
   const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port:  465,
   secure: false,
     auth: {
       user: process.env.EMAIL_USER,
